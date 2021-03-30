b3a058a828a45cf466f7229c89dc49c392d1d5e4
Alejandro-sin <alejandro.chem.io@gmail.com>
2021-03-30 08:08:57 -0500

    update vhr

--- src/record.py
+++ src/record.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import cv2
+import time
 
 
 '''
@@ -27,23 +28,34 @@ class Caps():
 
 
 def video_cap():
+    capture_duration = 11
     capture = cv2.VideoCapture(0)
     # Encoding
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-
     # Write and save
     out = cv2.VideoWriter('./data/record.avi',fourcc, 20.0, (640,480))
+    start_time = time.time()
 
-    while(True):
+    while(int(time.time() - start_time) < capture_duration):
         ret, frame = capture.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        out.write(frame)
-        cv2.imshow('frame',gray)
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if ret ==True:
+            frame = cv2.flip(frame,0)
+            out.write(frame)
+            cv2.imshow('frame')  #('frame',gray)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
     capture.release()
     out.release()
     cv2.destroyAllWindows()
+
+
+
+
+
+
 
 
 
